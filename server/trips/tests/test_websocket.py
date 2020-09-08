ed9335fba199fb5d9a95a92e12e77bbcff0719a7
Jorge Contreras <jorge.contreras789@gmail.com>
2020-09-07 22:05:48 -0400

    added access token to query parameter for authenticating socket connection

--- server/trips/tests/test_websocket.py
+++ server/trips/tests/test_websocket.py
@@ -1,6 +1,10 @@
+from django.contrib.auth import get_user_model
+from django.db import transaction
 import pytest
 from channels.testing import WebsocketCommunicator
 from channels.layers import get_channel_layer
+from channels.db import database_sync_to_async
+from rest_framework_simplejwt.tokens import AccessToken
 
 from taxi.routing import application
 
@@ -11,11 +15,25 @@ TEST_CHANNEL_LAYERS = {
 }
 
 
+@database_sync_to_async
+def create_user(username, password):
+    user = get_user_model().objects.create_user(
+        username=username,
+        password=password,
+    )
+    access = AccessToken.for_user(user)
+    return user, access
+
+
 @pytest.mark.asyncio
+@pytest.mark.django_db(transaction=True)
 class TestWebSocket:
     async def test_can_connect_to_server(self, settings):
         settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
-        communicator = WebsocketCommunicator(application=application, path="/taxi/")
+        _, access = await create_user("test.user@example.com", "pAssw0rd")
+        communicator = WebsocketCommunicator(
+            application=application, path=f"/taxi/?token={access}"
+        )
         connected, _ = await communicator.connect()
         assert connected is True
         await communicator.disconnect()
@@ -45,4 +63,11 @@ class TestWebSocket:
         await channel_layer.group_send("test", message=message)
         response = await communicator.receive_json_from()
         assert response == message
+        await communicator.disconnect()
+
+    async def test_cannot_connect_to_socket(self, settings):
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        communicator = WebsocketCommunicator(application=application, path="/taxi/")
+        connected, _ = await communicator.connect()
+        assert connected is False
         await communicator.disconnect()
