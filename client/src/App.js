852227250ed318702df85b190fcbdbb68fab8e9a
Jorge Contreras <jorge.contreras789@gmail.com>
2020-09-10 12:51:23 -0400

    initial router and cypress setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Route, Link, Switch } from 'react-router-dom';
+
+import SignUp from './components/SignUp';
+import LogIn from './components/LogIn';
 
 import './App.css';
 
 function App() {
-  return <h1>Taxi</h1>;
+  return (
+    <Switch>
+      <Route
+        exact
+        path="/"
+        render={() => (
+          <div>
+            <h1>Taxi</h1>
+            <Link to="/sign-up">Sign up</Link>
+            <Link to="/log-in">Log in</Link>
+          </div>
+        )}
+      />
+      <Route path="/sign-up" component={SignUp} />
+      <Route path="/log-in" component={LogIn} />
+    </Switch>
+  );
 }
 
 export default App;
