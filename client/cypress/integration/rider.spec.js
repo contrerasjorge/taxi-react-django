274dadc3d30f88213497e8153587f7908f7f5a7f
Jorge Contreras <jorge.contreras789@gmail.com>
2020-09-15 11:29:54 -0400

    created rider dashboard

--- client/cypress/integration/rider.spec.js
+++ client/cypress/integration/rider.spec.js
@@ -109,4 +109,22 @@ describe('The rider dashboard', function () {
     // Completed trips.
     cy.get('[data-cy=trip-card]').eq(1).contains('COMPLETED');
   });
+
+  it('Shows details about a trip', () => {
+    cy.server();
+    cy.route({
+      method: 'GET',
+      url: '**/api/trip/**',
+      status: 200,
+      response: tripResponse[0],
+    }).as('getTrips');
+
+    cy.logIn(riderEmail);
+
+    cy.visit(`/#/rider/${tripResponse[0].id}`);
+
+    cy.get('[data-cy=trip-card]')
+      .should('have.length', 1)
+      .and('contain.text', 'STARTED');
+  });
 });
